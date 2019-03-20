09cca56501f91ef645fd45d7a9dd0ed2717ff8cd
joshbantayan <joshuabantayan31@gmail.com>
2019-03-20 23:15:59 +0800

    added initial code for itemController.js

--- api/controllers/itemController.js
+++ api/controllers/itemController.js
@@ -1,3 +1,23 @@
+const mongoose = require('mongoose');
+const User = require('../models/User');
+const Items = require('../models/Items');
+
+module.exports = {
+	addItem : () => {
+
+	}
+	deleteItem : () => {
+
+	}
+	getItem : () => {
+
+	}
+	updateItem : () => {
+		
+	}
+}
+
+
 // methods in itemController:
 // -addItem
 // -deleteItem
